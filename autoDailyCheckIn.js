732280ac401c3a376a7d10f12709f477b691557d
OrigFifty <105519374+OrigFifty@users.noreply.github.com>
2024-11-02 10:51:44 +0100

    Fixed
    Fixed and added Tears of Themis

--- autoDailyCheckIn.js
+++ autoDailyCheckIn.js
@@ -2,16 +2,17 @@ const profiles = [
     {
         token: "ltoken_v2=v2_XXXXX; ltuid_v2=XXXXX;",
         zzz: true,
-        genshin: false,
-        honkai_star_rail: false,
-        honkai_3: false,
-        accountName: "ADD UNQIUE IDENTIFIER FOR YOUR ACCOUNT HERE"
+        genshin: true,
+        honkai_star_rail: true,
+        honkai_3: true,
+        tears_of_themis: true,
+        accountName: "ADD UNIQUE IDENTIFIER FOR YOUR ACCOUNT HERE"
     }
 ];
 
 /** Discord Notification **/
 const discord_notify = false;
-const myDiscordID = "INSERT YOUR DISCORD ID IF YOU WANT TO GET PING, OR YOU CAN LEAVE IT EMPTY";
+const myDiscordID = "INSERT YOUR DISCORD ID IF YOU WANT TO GET PINGED, OR YOU CAN LEAVE IT EMPTY";
 const discordWebhook =
     "INSERT YOUR WEBHOOK HERE";
 
@@ -20,13 +21,15 @@ const discordWebhook =
 
 const urlDict = {
     ZZZ:
-        "https://sg-act-nap-api.hoyolab.com/event/luna/zzz/os/sign?lang=en-us&act_id=e202406031448091",
+        "https://sg-public-api.hoyolab.com/event/luna/zzz/os/sign?lang=en-us&act_id=e202406031448091",
     Genshin:
         "https://sg-hk4e-api.hoyolab.com/event/sol/sign?lang=en-us&act_id=e202102251931481",
     Star_Rail:
         "https://sg-public-api.hoyolab.com/event/luna/os/sign?lang=en-us&act_id=e202303301540311",
     Honkai_3:
-        "https://sg-public-api.hoyolab.com/event/mani/sign?lang=en-us&act_id=e202110291205111"
+        "https://sg-public-api.hoyolab.com/event/mani/sign?lang=en-us&act_id=e202110291205111",
+    Tears_of_Themis:
+        "https://sg-public-api.hoyolab.com/event/luna/nxx/os/sign?lang=en-us&act_id=e202202281857121"
 };
 
 async function main() {
@@ -48,13 +51,14 @@ function discordPing() {
     }
 }
 
-function autoSignFunction({ token, zzz, genshin, honkai_star_rail, honkai_3, accountName }) {
+async function autoSignFunction({ token, zzz, genshin, honkai_star_rail, honkai_3, tears_of_themis, accountName }) {
     const urls = [];
 
-    if (zzz) urls.push(urlDict.ZZZ);
-    if (genshin) urls.push(urlDict.Genshin);
-    if (honkai_star_rail) urls.push(urlDict.Star_Rail);
-    if (honkai_3) urls.push(urlDict.Honkai_3);
+    if (zzz) urls.push({ url: urlDict.ZZZ, signgame: "zzz" });
+    if (genshin) urls.push({ url: urlDict.Genshin, signgame: "genshin" });
+    if (honkai_star_rail) urls.push({ url: urlDict.Star_Rail, signgame: "hsr" });
+    if (honkai_3) urls.push({ url: urlDict.Honkai_3, signgame: "honkai3rd" });
+    if (tears_of_themis) urls.push({ url: urlDict.Tears_of_Themis, signgame: "tot" });
 
     const header = {
         Cookie: token,
@@ -64,13 +68,9 @@ function autoSignFunction({ token, zzz, genshin, honkai_star_rail, honkai_3, acc
         "x-rpc-app_version": "2.34.1",
         "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-        "x-rpc-client_type": "4",
         Referer: "https://act.hoyolab.com/",
         Origin: "https://act.hoyolab.com"
     };
-
-    if (zzz) header["x-rpc-signgame"] = "zzz";
-    if (honkai_star_rail) header["x-rpc-signgame"] = "hsr";
 
     const options = {
         method: "POST",
@@ -82,12 +82,12 @@ function autoSignFunction({ token, zzz, genshin, honkai_star_rail, honkai_3, acc
     let retry = false;
 
     do {
-        const httpResponses = UrlFetchApp.fetchAll(urls.map(url => ({ url, ...options })));
-
-        for (const [i, hoyolabResponse] of httpResponses.entries()) {
+        for (const { url, signgame } of urls) {
+            header["x-rpc-signgame"] = signgame; // Set signgame specific to each game
+            const hoyolabResponse = UrlFetchApp.fetch(url, options);
             const checkInResult = JSON.parse(hoyolabResponse).message;
             const gameName = Object.keys(urlDict)
-                .find(key => urlDict[key] === urls[i])
+                .find(key => urlDict[key] === url)
                 ?.replace(/_/g, " ");
             const isError = checkInResult != "OK";
             const bannedCheck = JSON.parse(hoyolabResponse).data?.gt_result?.is_risk;
@@ -99,6 +99,7 @@ function autoSignFunction({ token, zzz, genshin, honkai_star_rail, honkai_3, acc
             } else {
                 response += `\n${gameName}: ${isError ? discordPing() : ""}${checkInResult}`;
             }
+
         }
 
         // If any request failed due to CAPTCHA, retry in an hour
